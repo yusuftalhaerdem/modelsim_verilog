9f4b52b67f5fe480209e63ccbc075a3a1d80c17a
Sena Dilara Yangöz <66904315+senayngz@users.noreply.github.com>
2021-06-13 17:41:16 +0300

    Delete singlecycleMIPS-lite-commented directory

--- singlecycleMIPS-lite-commented/shift.v
+++
@@ -1,5 +0,0 @@
-module shift(shout,shin);
-output [31:0] shout;
-input [31:0] shin;
-assign shout=shin<<2;
-endmodule
--- singlecycleMIPS-lite-commented/signext.v
+++
@@ -1,5 +0,0 @@
-module signext(in1,out1);
-input [15:0] in1;
-output [31:0] out1;
-assign 	 out1 = {{ 16 {in1[15]}}, in1};
-endmodule
