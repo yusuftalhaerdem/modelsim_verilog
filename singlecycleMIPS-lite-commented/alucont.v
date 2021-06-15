70c6f6632c225e12d6f8bf92726b53cc1e9fddd5
Sena Dilara Yangöz <66904315+senayngz@users.noreply.github.com>
2021-06-15 14:52:15 +0300

    nori added

--- singlecycleMIPS-lite-commented/alucont.v
+++ singlecycleMIPS-lite-commented/alucont.v
@@ -4,22 +4,18 @@ output [2:0] gout;
 reg [2:0] gout;
 always @(aluop1 or aluop0 or f3 or f2 or f1 or f0)
 begin
-if(~(aluop1|aluop0))  gout=3'b010;
-if(aluop0)gout=3'b110;
-if(aluop1)//R-type
-begin
-	// add new control signals are here 	// we may need to change other codes here
-	//	8		4		2		1
-	//nori
-//	if ( f3&	f2&		f1&		(~f0))gout = b'???
-	//sllv
-//	if ((~f3)&	f2&		(~f1)&	(~f0))gout = b'???
 
+if(aluop1&aluop0)gout=3'b100;		//ALU control=100 (nori)
+if(~(aluop1|aluop0))gout=3'b010;
+if((~aluop1)&aluop0)gout=3'b110;
+if(aluop1&(~aluop0))//R-type
+begin
 	if (~(f3|f2|f1|f0))gout=3'b010; 	//function code=0000,ALU control=010 (add)
-	if (f1&f3)gout=3'b111;			//function code=1x1x,ALU control=111 (set on less than)
-	if (f1&~(f3))gout=3'b110;		//function code=0x10,ALU control=110 (sub)
-	if (f2&f0)gout=3'b001;			//function code=x1x1,ALU control=001 (or)
-	if (f2&~(f0))gout=3'b000;		//function code=x1x0,ALU control=000 (and)
+	if (f1&f3&(~f2)&(~f0))gout=3'b111;			//function code=1010,ALU control=111 (set on less than)
+	if (f1&~(f3)&~(f2)&~(f0))gout=3'b110;		//function code=0010,ALU control=110 (sub)
+	if (f2&f0&~(f3)&~(f1))gout=3'b001;			//function code=0101,ALU control=001 (or)
+	if (f2&~(f0)&~(f1)&~(f3))gout=3'b000;		//function code=0100,ALU control=000 (and)
+	
 end
 end
 endmodule
